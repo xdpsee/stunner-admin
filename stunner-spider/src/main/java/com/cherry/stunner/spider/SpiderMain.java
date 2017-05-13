b896170bd4d7a6bf5aeca5173e11944ffc4dcb11
cherry <xdpsee@gmail.com>
2017-05-13 22:42:40 +0800

    Init Projects

---
+++ stunner-spider/src/main/java/com/cherry/stunner/spider/SpiderMain.java
@@ -0,0 +1,15 @@
+package com.cherry.stunner.spider;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpiderMain {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("src/main/resources/application-context.xml");
+
+    }
+
+}
+
+
