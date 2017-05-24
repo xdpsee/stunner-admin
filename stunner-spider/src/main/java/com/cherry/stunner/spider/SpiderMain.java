a593d98f237f22b1a40d24eb3d168bb20f1ad788
cherry <xdpsee@gmail.com>
2017-05-24 08:23:19 +0800

    :

--- stunner-spider/src/main/java/com/cherry/stunner/spider/SpiderMain.java
+++ stunner-spider/src/main/java/com/cherry/stunner/spider/SpiderMain.java
@@ -1,12 +1,6 @@
 package com.cherry.stunner.spider;
 
-import com.cherry.stunner.spider.m86.M86CategoryTagsProcessor;
-import com.cherry.stunner.spider.m86.M86ModelTagsProcessor;
-import com.cherry.stunner.spider.m86.M86StarTagsProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
-import us.codecraft.webmagic.scheduler.component.HashSetDuplicateRemover;
 
 public class SpiderMain {
 
@@ -14,23 +8,23 @@ public class SpiderMain {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
-        Spider.create(context.getBean(M86CategoryTagsProcessor.class))
-                .addUrl("http://www.17786.com/meinv/")
-                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
-                .thread(1)
-                .run();
-
-        Spider.create(context.getBean(M86ModelTagsProcessor.class))
-                .addUrl("http://www.17786.com/mtdq/")
-                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
-                .thread(1)
-                .run();
-
-        Spider.create(context.getBean(M86StarTagsProcessor.class))
-                .addUrl("http://www.17786.com/mxdq/")
-                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
-                .thread(1)
-                .run();
+//        Spider.create(context.getBean(M86CategoryTagsProcessor.class))
+//                .addUrl("http://www.17786.com/meinv/")
+//                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
+//                .thread(1)
+//                .run();
+//
+//        Spider.create(context.getBean(M86ModelTagsProcessor.class))
+//                .addUrl("http://www.17786.com/mtdq/")
+//                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
+//                .thread(1)
+//                .run();
+//
+//        Spider.create(context.getBean(M86StarTagsProcessor.class))
+//                .addUrl("http://www.17786.com/mxdq/")
+//                .setScheduler(new FileCacheQueueScheduler("./").setDuplicateRemover(new HashSetDuplicateRemover()))
+//                .thread(1)
+//                .run();
 
     }
 
