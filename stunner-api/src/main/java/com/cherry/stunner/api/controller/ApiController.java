65972da2a6e1dd95fcc58c41c264679c74d008bb
cherry <xdpsee@gmail.com>
2017-05-29 12:19:46 +0800

    :spring security - 1

---
+++ stunner-api/src/main/java/com/cherry/stunner/api/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.cherry.stunner.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+
+
+}
