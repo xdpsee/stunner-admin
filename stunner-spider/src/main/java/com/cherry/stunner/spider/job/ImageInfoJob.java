d6e8fe0e1dc3dd74aaffdcdea496b3274c2fad3f
cherry <xdpsee@gmail.com>
2017-05-30 13:40:26 +0800

    :quartz job

---
+++ stunner-spider/src/main/java/com/cherry/stunner/spider/job/ImageInfoJob.java
@@ -0,0 +1,12 @@
+package com.cherry.stunner.spider.job;
+
+public class ImageInfoJob {
+
+    public void execute() {
+
+        System.out.println(ImageInfoJob.class.getName());
+
+
+    }
+
+}
---
+++ stunner-spider/src/main/java/com/cherry/stunner/spider/job/SpiderJob.java
@@ -0,0 +1,11 @@
+package com.cherry.stunner.spider.job;
+
+public class SpiderJob {
+
+    public void execute() {
+
+        System.out.println(SpiderJob.class.getName());
+
+    }
+
+}
